f628b5225479149fb3ffbd60074e03e95222990a
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-08-02 22:17:00 -0500

    Clases y estilos.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,10 +10,53 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ng-store';
 
+  widthImg = 10;
+
   person = {
     age: 21,
-    name: 'John Doe'
+    name: 'John Doe',
+    image: 'https://www.w3schools.com/howto/img_avatar.png'
   };
+
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ];
 
   onScroll(event: Event): void {
     const scroll = event.target as HTMLElement;
