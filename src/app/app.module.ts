553dbba4cd123feb6ed90d649e9c7873913241a7
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-25 11:28:15 -0500

    Se configura un service worker.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { QuicklinkModule } from 'ngx-quicklink';
@@ -27,6 +28,12 @@ import { environment } from '../environments/environment';
     QuicklinkModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
+    ServiceWorkerModule.register('ngsw-worker.js', { // https://angular.io/guide/service-worker-getting-started
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   declarations: [
     AppComponent,
